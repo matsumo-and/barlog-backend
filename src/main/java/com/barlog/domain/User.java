57c94c366f8bbc14d16f68aa1de120a85aa85d45
matsumo-and <yh134.toisanda@gmail.com>
2023-12-09 00:05:28 +0900

    initial commit

---
+++ src/main/java/com/barlog/domain/User.java
@@ -0,0 +1,6 @@
+package com.barlog.domain;
+
+public class User {
+    String id;
+    String name;
+}
---
+++ src/main/java/com/barlog/interfaces/presenter/Hello.java
@@ -0,0 +1,12 @@
+package com.barlog.interfaces.presenter;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+public class Hello {
+    String aa;
+
+    public void init(){
+        // some
+    }
+}
